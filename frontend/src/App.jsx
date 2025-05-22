f7a700adf50dc01a0ab50768560c2b42c2359690
karan8904 <karanchavda210@gmail.com>
2025-05-22 09:43:08 +0530

    First Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button className="btn btn-primary">Primary</button>
+    </>
+  )
+}
+
+export default App
