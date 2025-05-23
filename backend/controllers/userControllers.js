0f56239a765483a7e333f74b86033dcd0f8bf719
karan8904 <karanchavda210@gmail.com>
2025-05-23 17:25:38 +0530

    created login user and get user routes

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,5 +1,6 @@
 import User from '../models/userSchema.js'
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 export const createUser = async (req, res) => {
     try {
@@ -14,5 +15,40 @@ export const createUser = async (req, res) => {
         res.status(201).json({ message: "User Created.", user: user })
     } catch (error) {
         res.status(500).json({ message: "Cannot create a user.", error: error.message })
+    }
+}
+
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({ email })
+        if(!user){
+            return res.status(404).json({ message: "User with given email does not exist." })
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.status(400).json({ message: "Password is incorrect. Try again."})
+        }
+        const token = jwt.sign({
+                userId: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin
+            },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: '30d' }
+        )
+        res.status(200).json({ message: "Logged in successfully.", token, user: user})
+    } catch (error) {
+        res.status(400).json({ message: "Login failed." }) 
+    }
+}
+
+export const getUsers = async (req, res) => {
+    try {
+        const users = await User.find()
+        res.status(200).json({ users: users })
+    } catch (error) {
+        res.status(500).json({ message: "Cannot get users.", error: error.message })
     }
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { createUser } from '../controllers/userControllers.js'
+import { createUser, getUsers, loginUser } from '../controllers/userControllers.js'
 
 const user_router = express.Router()
 
+user_router.get('/', getUsers)
 user_router.post('/create', createUser)
+user_router.post('/login', loginUser)
 
 export default user_router
